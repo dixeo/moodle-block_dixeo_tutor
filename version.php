dbe5cbd5b3964541369b0826f00783a116c03bb4
Josemaria Bolanos <josemabol@gmail.com>
2026-06-17 16:28:25 -0400

    Add cancellation button in setup panels

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026073105;
+$plugin->version   = 2026073106;
 $plugin->requires  = 2024100700;
 $plugin->component = 'block_dixeo_tutor';
 $plugin->maturity  = MATURITY_STABLE;
